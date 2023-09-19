fdba24d89577556c023fe7bcd9ded65574c8d5cc
Julio Martinez <71898558+juliomtz00@users.noreply.github.com>
2023-09-19 01:37:01 -0600

    Update and rename Practica1.c to p1.c

--- p1.c
+++ p1.c
@@ -13,54 +13,54 @@ void main() {
  oldstate1=0;
  oldstate2=0;
   do {
-    if (Button(&PORTA, 0, 1, 1)) {               // Detect logical one del Botón 1
+    if (Button(&PORTA, 0, 1, 1)) {               // Detect logical one del BotÃ³n 1
       oldstate = 1;                              // Update flag
     }
     if (oldstate && Button(&PORTA, 0, 1, 0)) {   // Detect one-to-zero transition
       PORTB.F0 = ~PORTB.F0;                              // Invert PORTB.F0
       oldstate = 0;                              // Update flag
     }
-    if(PORTB.F0==1){    //Si ya se presionó el botón 1, se puede elegir la temporización
-      if (Button(&PORTA, 1, 1, 1)) {               // Detect logical one del Botón 2
+    if(PORTB.F0==1){    //Si ya se presionÃ³ el botÃ³n 1, se puede elegir la temporizaciÃ³n
+      if (Button(&PORTA, 1, 1, 1)) {               // Detect logical one del BotÃ³n 2
       oldstate1 = 1;                              // Update flag
       }
       if (oldstate1 && Button(&PORTA, 1, 1, 0)) {   // Detect one-to-zero transition
       oldstate1 = 0;
       PORTB.F1=1;
-        do{     //Primera opción de 1hora (10s)
+        do{     //Primera opciÃ³n de 1hora (10s)
         i++;
         time=i;
         delay_ms(1);
-        if (Button(&PORTA, 1, 1, 1)) {               // Detect logical one del Botón 2 (2da presión)
+        if (Button(&PORTA, 1, 1, 1)) {               // Detect logical one del BotÃ³n 2 (2da presiÃ³n)
            oldstate2 = 1;                              // Update flag
         }
         if (oldstate2 && Button(&PORTA, 1, 1, 0)) {   // Detect one-to-zero transition
            PORTB.F1=0;
            PORTB.F2=1;
            oldstate2=0;
-           i=2000; // al realizar la segunda presión la cuenta se detiene, el ciclo termina
+           i=2000; // al realizar la segunda presiÃ³n la cuenta se detiene, el ciclo termina
         }
-        }while(i<2000); //Se cuenta hasta 2000, con esto se conseguió un tiempo similar a 10s
+        }while(i<2000); //Se cuenta hasta 2000, con esto se conseguiÃ³ un tiempo similar a 10s
         PORTB.F1=0;
         if (time==2000){
         PORTB.F0=0;//Si pasa el tiempo completo el humidificador se apaga por completo
         }
         i=time;
-        if (PORTB.F2==1){ //Segunda opción 2 horas (20s)
+        if (PORTB.F2==1){ //Segunda opciÃ³n 2 horas (20s)
           do{
           i++;
           time1=i;
           delay_ms(1);
-          if (Button(&PORTA, 1, 1, 1)) {   // Detect logical one del Botón 2 (3ra presión)
+          if (Button(&PORTA, 1, 1, 1)) {   // Detect logical one del BotÃ³n 2 (3ra presiÃ³n)
              oldstate3 = 1;                              // Update flag
           }
           if (oldstate3 && Button(&PORTA, 1, 1, 0)) {   // Detect one-to-zero transition
             PORTB.F1=0;
             PORTB.F2=0;
             oldstate3=0;
-            i=4000; //al realizar la tercera presión la cuenta se detiene, el ciclo termina
+            i=4000; //al realizar la tercera presiÃ³n la cuenta se detiene, el ciclo termina
           }
-          }while(i<4000); //Se cuenta hasta 4000, con esto se conseguió un tiempo similar a 20s
+          }while(i<4000); //Se cuenta hasta 4000, con esto se conseguiÃ³ un tiempo similar a 20s
       i=0;
       PORTB.F2=0; //
       if (time1==4000){
